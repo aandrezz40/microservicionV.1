e1bb26ed154f85b30171970d5e0313715f02d991
jhojan <aandrezz40@gmail.com>
2025-10-17 09:54:46 -0500

    fd

--- config-service/src/main/java/com/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/config/service/ConfigServiceApplication.java
@@ -1,10 +1,12 @@
 package com.config.service;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfig
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
